341ca5d910a3b4ce549b77dd2b191fc44639b770
Antoni Boucher <bouanto@zoho.com>
2022-12-30 10:14:10 -0500

    Remove thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,15 +1,42 @@
+use std::fmt::Display;
+
 pub type Result<T = ()> = std::result::Result<T, Error>;
 
-#[derive(Debug, Eq, PartialEq, thiserror::Error)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Error {
-    #[error("{0}")]
-    ParseIntError(#[from] std::num::ParseIntError),
-    #[error("{0}")]
-    NulError(#[from] std::ffi::NulError),
-    #[error("{0}")]
+    ParseIntError(std::num::ParseIntError),
+    NulError(std::ffi::NulError),
     Backend(String),
-    #[error("Unknow error")]
     Unknow,
-    #[error("{0}")]
-    Utf8(#[from] std::str::Utf8Error),
+    Utf8(std::str::Utf8Error),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Error::ParseIntError(ref error) => error.fmt(f),
+            Error::NulError(ref error) => error.fmt(f),
+            Error::Backend(ref error) => error.fmt(f),
+            Error::Unknow => "Unknown error".fmt(f),
+            Error::Utf8(ref error) => error.fmt(f),
+        }
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::ParseIntError(error)
+    }
+}
+
+impl From<std::ffi::NulError> for Error {
+    fn from(error: std::ffi::NulError) -> Self {
+        Error::NulError(error)
+    }
+}
+
+impl From<std::str::Utf8Error> for Error {
+    fn from(error: std::str::Utf8Error) -> Self {
+        Error::Utf8(error)
+    }
 }
